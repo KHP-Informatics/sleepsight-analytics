9c840712ee27bd24460b33f19094d7dce28b4c77
Maximilian Kerz <kerz.maximilian@gmail.com>
2017-06-11 22:18:06 +0200

    IndexError DTW fix

--- analysis/model.py
+++ analysis/model.py
@@ -241,26 +241,27 @@ class NonParaModel:
         indexStart = index['indexStart']
         indexEnd = index['indexEnd']
         indexPrev = indexStart - 1440
-        dfData = self.xData.loc[indexStart:indexEnd]
-        dfDataPrev = self.xData.loc[indexPrev:indexStart]
-        dtwDist = []
-        for feature in self.xFeatures:
-            ts = np.array(list(dfData[feature]))
-            tsPrev = np.array(list(dfDataPrev[feature]))
-            dist, path = self.DTW(ts, tsPrev)
-            dtwDist.append(dist)
+        try:
+            dfData = self.xData.loc[indexStart:indexEnd]
+            dfDataPrev = self.xData.loc[indexPrev:indexStart]
+            dtwDist = []
+            for feature in self.xFeatures:
+                ts = np.array(list(dfData[feature]))
+                tsPrev = np.array(list(dfDataPrev[feature]))
+                dist, path = self.DTW(ts, tsPrev)
+                dtwDist.append(dist)
+        except IndexError:
+            dtwDist = ['NaN'] * len(self.xFeatures)
         return dtwDist
 
     def DTW(self, A, B,  window=sys.maxsize, d=lambda x, y: abs(x - y)):
         # create the cost matrix
         M, N = len(A), len(B)
         cost = sys.maxsize * np.ones((M, N))
-
         # initialize the first row and column
         cost[0, 0] = d(A[0], B[0])
         for i in range(1, M):
             cost[i, 0] = cost[i - 1, 0] + d(A[i], B[0])
-
         for j in range(1, N):
             cost[0, j] = cost[0, j - 1] + d(A[0], B[j])
         # fill in the rest of the matrix
@@ -268,15 +269,12 @@ class NonParaModel:
             for j in range(max(1, i - window), min(N, i + window)):
                 choices = cost[i - 1, j - 1], cost[i, j - 1], cost[i - 1, j]
                 cost[i, j] = min(choices) + d(A[i], B[j])
-
         # find the optimal path
         n, m = N - 1, M - 1
         path = []
-
         while (m, n) != (0, 0):
             path.append((m, n))
             m, n = min((m - 1, n), (m, n - 1), (m - 1, n - 1), key=lambda x: cost[x[0], x[1]])
-
         path.append((0, 0))
         return cost[-1, -1], path
 
