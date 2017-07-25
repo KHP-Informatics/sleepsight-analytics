bf51936042825967722cb8ea3c9b9edbd990fc7d
Maximilian Kerz <kerz.maximilian@gmail.com>
2017-07-25 15:17:29 +0200

    Rebalanced class - strip down

--- analysis/machlearn.py
+++ analysis/machlearn.py
@@ -135,13 +135,13 @@ class InfoGain:
 #from sklearn.datasets import make_classification
 #from imblearn.over_sampling import ADASYN
 #
-#class Rebalance:
-#
-#    def __init__(self, X, y):
-#        self.pdX = X
-#        self.pdY = y
-#        print(y)
-#
+class Rebalance:
+
+    def __init__(self, X, y):
+        self.pdX = X
+        self.pdY = y
+        print(y)
+
 #    def test(self):
 #        X, y = make_classification(n_classes=2, class_sep=2, weights = [0.1, 0.9], n_informative = 3, n_redundant = 1,
 #                                   flip_y = 0, n_features = 20, n_clusters_per_class = 1, n_samples = 1000, random_state = 10)
