58e5131babd5ae39e349aafa8a8d1b6d3adb3430
Maximilian Kerz <kerz.maximilian@gmail.com>
2017-07-25 15:15:03 +0200

    ISS-server: scikit dependency >= 0.17.0

--- analysis/machlearn.py
+++ analysis/machlearn.py
@@ -131,21 +131,21 @@ class InfoGain:
 # print(ig)
 ###############################################################################
 
-from collections import Counter
-from sklearn.datasets import make_classification
-from imblearn.over_sampling import ADASYN
-
-class Rebalance:
-
-    def __init__(self, X, y):
-        self.pdX = X
-        self.pdY = y
-        print(y)
-
-    def test(self):
-        X, y = make_classification(n_classes=2, class_sep=2, weights = [0.1, 0.9], n_informative = 3, n_redundant = 1,
-                                   flip_y = 0, n_features = 20, n_clusters_per_class = 1, n_samples = 1000, random_state = 10)
-        print('Original dataset shape {}'.format(Counter(y)))
-        ada = ADASYN(random_state=42)
-        X_res, y_res = ada.fit_sample(X, y)
-        print('Resampled dataset shape {}'.format(Counter(y_res)))
+#from collections import Counter
+#from sklearn.datasets import make_classification
+#from imblearn.over_sampling import ADASYN
+#
+#class Rebalance:
+#
+#    def __init__(self, X, y):
+#        self.pdX = X
+#        self.pdY = y
+#        print(y)
+#
+#    def test(self):
+#        X, y = make_classification(n_classes=2, class_sep=2, weights = [0.1, 0.9], n_informative = 3, n_redundant = 1,
+#                                   flip_y = 0, n_features = 20, n_clusters_per_class = 1, n_samples = 1000, random_state = 10)
+#        print('Original dataset shape {}'.format(Counter(y)))
+#        ada = ADASYN(random_state=42)
+#        X_res, y_res = ada.fit_sample(X, y)
+#        print('Resampled dataset shape {}'.format(Counter(y_res)))
